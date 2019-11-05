c5a554b55366f68dcd7ceb2d00449f47c64da724
Blazingsteal <32238392+Blazingsteal@users.noreply.github.com>
2019-11-05 12:30:12 +0100

    added more functionality to calendar

--- calendar.js
+++ calendar.js
@@ -1,26 +1,48 @@
-function next(){
-    if (currentMonth === 11){
+let today = new date()
+let currentMonth = today.getMonth()
+let currentYear = today.getFullYear()
+let selectedYear = document.getElementById("year")
+let selectedMonth = document.getElementById("month")
+
+let months = ["January", "February", "Marts", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+let monthAndYear = document.getElementById("newName")
+createCalendar(currentMonth, currentYear)
+
+
+function next() {
+    if (currentMonth === 11) {
         currentYear = currentYear + 1
     }
     currentMonth = (currentMonth + 1) % 12
-    showCalendar(currentMonth, currentYear)
+    createCalendar(currentMonth, currentYear)
 }
 
-function previous(){
+function previous() {
 
-    if (currentMonth === 0){
+    if (currentMonth === 0) {
         currentYear = currentYear - 1
         currentMonth = 11
     }
-    else if (currentMonth != 0){
+    else if (currentMonth != 0) {
         currentMonth = currentMonth - 1
     }
-    showCalendar(currentMonth, currentYear)
+    createCalendar(currentMonth, currentYear)
 }
 
 function jump() {
     //Jumps to selected year and month
-    currentYear = parseInt(selectYear.value)
-    currentMonth = parseInt(selectMonth.value)
-    showCalendar(currentMonth, currentYear)
+    currentYear = parseInt(selectedYear.value)
+    currentMonth = parseInt(selectedMonth.value)
+    createCalendar(currentMonth, currentYear)
+}
+
+function createCalendar(month, year) {
+    
+    
+    let date = new Date()
+    let firstDay = (new Date(year, month)).getDay()
+    let daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+
+
 }
