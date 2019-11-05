bfb60af996694b4be69d726b3c7715bea3c07e74
Blazingsteal <32238392+Blazingsteal@users.noreply.github.com>
2019-11-05 11:48:08 +0100

    added next, previous, and jump functions to calendar

--- calendar.js
+++ calendar.js
@@ -1,7 +1,26 @@
 function next(){
-    if (currentYear === 11){
-        currentYear + 1
+    if (currentMonth === 11){
+        currentYear = currentYear + 1
     }
-    (currentMonth + 1) % 12
+    currentMonth = (currentMonth + 1) % 12
+    showCalendar(currentMonth, currentYear)
+}
+
+function previous(){
+
+    if (currentMonth === 0){
+        currentYear = currentYear - 1
+        currentMonth = 11
+    }
+    else if (currentMonth != 0){
+        currentMonth = currentMonth - 1
+    }
+    showCalendar(currentMonth, currentYear)
+}
+
+function jump() {
+    //Jumps to selected year and month
+    currentYear = parseInt(selectYear.value)
+    currentMonth = parseInt(selectMonth.value)
     showCalendar(currentMonth, currentYear)
 }
