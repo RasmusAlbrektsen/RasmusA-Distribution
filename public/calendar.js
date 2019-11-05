e84d00d2bcb806f06b7dfd04b48761cdc3bea9f5
Blazingsteal <32238392+Blazingsteal@users.noreply.github.com>
2019-11-05 21:58:32 +0100

    it still works

--- public/calendar.js
+++ public/calendar.js
@@ -3,99 +3,109 @@ let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 let selectedYear = document.getElementById("year");
 let selectedMonth = document.getElementById("month");
-var markedDay;
+var markedDay = 'benis';
 
 let months = [
-  "January",
-  "February",
-  "Marts",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
+    "January",
+    "February",
+    "Marts",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
 ];
 
 let monthAndYear = document.getElementById("monthAndYear");
 createCalendar(currentMonth, currentYear);
 
 function next() {
-  if (currentMonth === 11) {
-    currentYear = currentYear + 1;
-  }
-  currentMonth = (currentMonth + 1) % 12;
-  createCalendar(currentMonth, currentYear);
+    if (currentMonth === 11) {
+        currentYear = currentYear + 1;
+    }
+    currentMonth = (currentMonth + 1) % 12;
+    createCalendar(currentMonth, currentYear);
 }
 
 function previous() {
-  if (currentMonth === 0) {
-    currentYear = currentYear - 1;
-    currentMonth = 11;
-  } else if (currentMonth != 0) {
-    currentMonth = currentMonth - 1;
-  }
-  createCalendar(currentMonth, currentYear);
+    if (currentMonth === 0) {
+        currentYear = currentYear - 1;
+        currentMonth = 11;
+    } else if (currentMonth != 0) {
+        currentMonth = currentMonth - 1;
+    }
+    createCalendar(currentMonth, currentYear);
 }
 
 function jump() {
-  //Jumps to selected year and month
-  currentYear = parseInt(selectedYear.value);
-  currentMonth = parseInt(selectedMonth.value);
-  createCalendar(currentMonth, currentYear);
+    //Jumps to selected year and month
+    currentYear = parseInt(selectedYear.value);
+    currentMonth = parseInt(selectedMonth.value);
+    createCalendar(currentMonth, currentYear);
 }
 
 function createCalendar(month, year) {
-  let startDay = (new Date(year, month)).getDay()
-  
-  //Sets monday as 0 instead of sunday
-  let mondayFirstDay = (startDay === 0 ? 6 : startDay -1);
-  
-  let daysInMonth = (new Date(year, month + 1, 0)).getDate()
+    let startDay = (new Date(year, month)).getDay()
 
-  // The last day of the previous month when comparing to the current month.
-  let prevLastDay = (new Date(year, month, 0)).getDate()
+    //Sets monday as 0 instead of sunday
+    let mondayFirstDay = (startDay === 0 ? 6 : startDay - 1);
 
-  let calendar = document.getElementById("calendar");
-  calendar.innerHTML = "";
+    let daysInMonth = (new Date(year, month + 1, 0)).getDate()
 
-  monthAndYear.innerHTML = months[month] + " " + year;
+    // The last day of the previous month when comparing to the current month.
+    let prevLastDay = (new Date(year, month, 0)).getDate()
 
-  let dateCounter = 1;
+    let calendar = document.getElementById("calendar");
+    calendar.innerHTML = "";
 
-  for (let i = 0; i <= 5; i++) {
-    let verticalRow = document.createElement("tr");
+    monthAndYear.innerHTML = months[month] + " " + year;
 
-    // Days from previous month, after subtracting the first day of current month
-    let prevMonthDays = prevLastDay - (mondayFirstDay - 1);
-    for (let j = 0; j <= 6; j++) {
-      if (i == 0 && prevMonthDays !== prevLastDay + 1) {
-        let dayCell = document.createElement("td");
-        let dayCellText = document.createTextNode(prevMonthDays);
-        dayCell.append(dayCellText);
-        verticalRow.append(dayCell);
-        prevMonthDays++;
-      } else if (dateCounter > daysInMonth) {
-        break;
-      } else {
-        let dayCell = document.createElement("td");
-        let dayCellText = document.createTextNode(dateCounter);
-        dayCell.append(dayCellText);
-        dayCell.onclick = function() {if(markedDay != null) {markedDay.removeAttribute("style"); markedDay = dayCell; console.log(markedDay); markedDay.style.backgroundColor = "#ffbc21";} else {markedDay = dayCell;}};
-        verticalRow.append(dayCell);
-        dateCounter++;
-      }
+    let dateCounter = 1;
+
+    for (let i = 0; i <= 5; i++) {
+        let verticalRow = document.createElement("tr");
+
+        // Days from previous month, after subtracting the first day of current month
+        let prevMonthDays = prevLastDay - (mondayFirstDay - 1);
+        for (let j = 0; j <= 6; j++) {
+            if (i == 0 && prevMonthDays !== prevLastDay + 1) {
+                let dayCell = document.createElement("td");
+                let dayCellText = document.createTextNode(prevMonthDays);
+                dayCell.append(dayCellText);
+                verticalRow.append(dayCell);
+                prevMonthDays++;
+            } else if (dateCounter > daysInMonth) {
+                break;
+            } else {
+                let dayCell = document.createElement("td");
+                let dayCellText = document.createTextNode(dateCounter);
+                dayCell.append(dayCellText);
+                dayCell.onclick = function () {
+                    if (markedDay != null) {
+                        markedDay.removeAttribute("style");
+                        markedDay = dayCell;
+                        console.log(markedDay);
+                        markedDay.style.backgroundColor = "#ffbc21";
+                    } else {
+                        markedDay = dayCell;
+                    }
+                };
+                verticalRow.append(dayCell);
+                dateCounter++;
+            }
+        }
+
+        calendar.appendChild(verticalRow);
     }
-
-    calendar.appendChild(verticalRow);
-  }
 }
 
 function highlight(cell) {
-  console.log(cell);
+    console.log(cell);
 }
 
-function createRows() {}
+function createRows() {
+}
--- public/scripts.js
+++ public/scripts.js
@@ -7,7 +7,7 @@ class Course {
 
 }
 
-// works lol just write it 
+// works lol just write it
 console.log(markedDay);
 
 class Deadline {
@@ -30,27 +30,15 @@ var list2 = list1.getElementsByTagName("li");
 var select = document.getElementById('selectList');
 
 
-for (i = 0; i < list2.length; i++) {
+for (let i = 0; i < list2.length; i++) {
     var option = document.createElement("option");
     option.text = list2[i].textContent;
     select.add(option);
 }
 
-function addDeadline() {
+function addDeadline(){
     var input = document.getElementById('deadlineInput');
-    var course = document.getElementByID('selectList');
-    if (input.value.length != 0) {
-        let d = new Deadline('markedDate', course.textContent, input)
-        var list = document.getElementById('deadlineList');
-
-
-    } else {
-        alert("Deadline description field is empty!");
-    }
-
-
-    var input = document.getElementById('deadlineInput');
-    if (input.value.length != 0) {
+    if(input.value.length != 0) {
         var list = document.getElementById('deadlineList');
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
@@ -61,20 +49,20 @@ function addDeadline() {
     }
 }
 
-function addClass() {
-    var input = document.getElementById('classInput');
-    if (input.value.length != 0) {
-        var list = document.getElementById('classList');
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        list.appendChild(li);
+// function addClass(){
+//   var input = document.getElementById('classInput');
+//   if(input.value.length != 0){
+//     var list = document.getElementById('classList');
+//     var li = document.createElement("li");
+//     li.appendChild(document.createTextNode(input.value));
+//     list.appendChild(li);
 
-        //Adding new class to select list
-        var option = document.createElement("option");
-        option.text = input.value;
-        input.value = "";
-        select.add(option);
-    } else {
-        alert("Class name field is empty!");
-    }
-}
+//     //Adding new class to select list
+//     var option = document.createElement("option");
+//     option.text = input.value;
+//     input.value = "";
+//     select.add(option);
+//   } else {
+//     alert("Class name field is empty!");
+//   }
+// }
