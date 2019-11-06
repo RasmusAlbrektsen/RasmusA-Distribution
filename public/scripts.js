8ca06f20e2d0d308ab2d4237df50ce60eea989d4
Blazingsteal <32238392+Blazingsteal@users.noreply.github.com>
2019-11-06 11:22:27 +0100

    able to create and add a course to the relevant lists

--- public/scripts.js
+++ public/scripts.js
@@ -9,7 +9,6 @@ class Course {
 }
 
 
-
 class Deadline {
 
     constructor(course, description) {
@@ -21,7 +20,7 @@ class Deadline {
 
 }
 
-
+var courseArray = [];
 
 fetch("data.json")
     .then(response => response.json())
@@ -30,36 +29,30 @@ fetch("data.json")
 
 var list1 = document.getElementById('classList');
 var list2 = list1.getElementsByTagName("li");
-var select = document.getElementById('selectList');
+var selectCourseList = document.getElementById('selectList');
 
 
 for (let i = 0; i < list2.length; i++) {
     var option = document.createElement("option");
     option.text = list2[i].textContent;
-    select.add(option);
+    selectCourseList.add(option);
 }
 
 function addDeadline() {
     var input = document.getElementById('deadlineInput');
     var courseList = document.getElementById('selectList')
     if (input.value.length != 0) {
-        //var deadlineList = document.getElementById('deadlineList');
-        d = new Deadline(courseList.options[courseList.selectedIndex].value, input.value);
-        console.log(d)
-
-
-    } else {
-        alert("Deadline description field is empty!");
-    }
-
-
-    var input = document.getElementById('deadlineInput');
-    if (input.value.length != 0) {
-        var list = document.getElementById('deadlineList');
+        var deadlineList = document.getElementById('deadlineList');
         var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        list.appendChild(li);
+
+        //creating a new deadline
+        d = new Deadline(courseList.options[courseList.selectedIndex].value, input.value);
+
+        //appending the new deadline to the deadline list
+        li.appendChild(document.createTextNode(d.course + ": " + d.description + " - " + d.day + " " + d.monthAndYearString));
+        deadlineList.appendChild(li);
         input.value = "";
+
     } else {
         alert("Deadline description field is empty!");
     }
@@ -68,16 +61,22 @@ function addDeadline() {
 function addClass() {
     var input = document.getElementById('classInput');
     if (input.value.length != 0) {
-        var list = document.getElementById('classList');
+        var classList = document.getElementById('classList');
         var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        list.appendChild(li);
+
+        //creating a new course
+        c = new Course(input.value)
+        courseArray.push(c)
+
+        //Appending the new course to the course list
+        li.appendChild(document.createTextNode(c.name));
+        classList.appendChild(li);
 
         //Adding new class to select list
         var option = document.createElement("option");
-        option.text = input.value;
+        option.text = c.name;
         input.value = "";
-        select.add(option);
+        selectCourseList.add(option);
     } else {
         alert("Class name field is empty!");
     }
